622ee11b99a1ca7bde5c7c1553eef5e77a27c863
Lj V. Miranda <ljvmiranda@gmail.com>
2021-08-23 20:13:22 +0800

    Add method to compute ray_color

--- src/Ray.ts
+++ src/Ray.ts
@@ -1,4 +1,5 @@
 import Vec3 from './Vec3'
+import Color from './Color'
 
 export default class Ray {
   orig: Vec3
@@ -11,5 +12,14 @@ export default class Ray {
 
   at(t: number) {
     return this.orig.add(this.dir.scale(t))
+  }
+
+  /* Return the color of the background (a simple gradient) */
+  color() {
+    let unitDirection = this.dir.unit()
+    let t = 0.5 * (unitDirection.y + 1.0)
+    return new Color(1.0, 1.0, 1.0)
+      .scale(1.0 - t)
+      .add(new Color(0.5, 0.7, 1.0).scale(t))
   }
 }
