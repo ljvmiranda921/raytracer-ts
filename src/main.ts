1fe2e7c502fd882375223fd6952a5639c7c617d3
Lj V. Miranda <ljvmiranda@gmail.com>
2021-08-23 20:23:50 +0800

    Update main function to use ray colors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import Color from './Color'
+import Vec3 from './Vec3'
+import Ray from './Ray'
 
-const imageWidth = 256
-const imageHeight = 256
+// Image settings
+const aspectRatio = 16.0 / 9.0
+const imageWidth = 400
+const imageHeight = imageWidth / aspectRatio
 
+// Camera settings
+const viewportHeight = 2.0
+const viewportWidth = aspectRatio * viewportHeight
+const focalLength = 1.0
+
+const origin = new Vec3(0, 0, 0)
+const horizontal = new Vec3(viewportWidth, 0, 0)
+const vertical = new Vec3(0, viewportHeight, 0)
+const lowerLeftCorner = origin
+  .subtract(horizontal.scale(0.5))
+  .subtract(vertical.scale(0.5))
+  .subtract(new Vec3(0, 0, focalLength))
+
+// Render
 console.log(`P3\n${imageWidth} ${imageHeight}\n255`)
 
 for (let j = imageHeight - 1; j >= 0; j--) {
@@ -11,8 +29,18 @@ for (let j = imageHeight - 1; j >= 0; j--) {
   process.stderr.write(`Scanlines remaining: ${j}`)
 
   for (let i = 0; i < imageWidth; i++) {
-    let color = new Color(i / (imageWidth - 1), j / (imageHeight - 1), 0.25)
-    color.write()
+    let u = i / imageWidth - 1
+    let v = j / imageHeight - 1
+    let r = new Ray(
+      origin,
+      lowerLeftCorner
+        .add(horizontal.scale(u))
+        .add(vertical.scale(v))
+        .subtract(origin)
+    )
+
+    let pixelColor = new Color(r.color().x, r.color().y, r.color().z)
+    pixelColor.write()
   }
 }
 
