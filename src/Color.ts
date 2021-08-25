fd3310271cd37fe917f1810a5657c2e24e7c09e3
Lj V. Miranda <ljvmiranda@gmail.com>
2021-08-25 19:11:06 +0800

    Update color writing to include clamp

--- src/Color.ts
+++ src/Color.ts
@@ -1,14 +1,33 @@
 import Vec3 from './Vec3'
 
 export default class Color extends Vec3 {
-    write() {
-        const ir = Math.round(255.999 * this.x)
-        const ig = Math.round(255.999 * this.y)
-        const ib = Math.round(255.999 * this.z)
+    write(samplesPerPixel: number) {
+        let r = this.x
+        let g = this.y
+        let b = this.z
+
+        const scale = 1.0 / samplesPerPixel
+        r *= scale
+        g *= scale
+        b *= scale
+
+        const ir = Math.floor(256 * clamp(r, 0.0, 0.999))
+        const ig = Math.floor(256 * clamp(g, 0.0, 0.999))
+        const ib = Math.floor(256 * clamp(b, 0.0, 0.999))
         console.log(`${ir} ${ig} ${ib}`)
     }
 
     static fromVec3(vec: Vec3): Color {
         return new Color(vec.x, vec.y, vec.z)
     }
+}
+
+function clamp(x: number, min: number, max: number): number {
+    if (x < min) {
+        return min
+    }
+    if (x > max) {
+        return max
+    }
+    return x
 }
